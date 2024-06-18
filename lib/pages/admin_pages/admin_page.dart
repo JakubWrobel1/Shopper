65fd4524beb36d5099dd609bc99445d10757b3f8
jakubw <wrobel.jakub01@gmail.com>
2024-06-18 21:07:37 +0200

    user edit

--- lib/pages/admin_pages/admin_page.dart
+++ lib/pages/admin_pages/admin_page.dart
@@ -11,8 +11,9 @@ class _AdminPageState extends State<AdminPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
 
-  void registerUser(String email, String password) async {
+  void registerUser(String email, String password, String name) async {
     try {
       UserCredential userCredential =
           await _auth.createUserWithEmailAndPassword(
@@ -25,7 +26,8 @@ class _AdminPageState extends State<AdminPage> {
       if (user != null) {
         await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
           'email': email,
-          'role': 'user', // Ustawianie roli użytkownika
+          'name': name,
+          'role': 'user',
         });
       }
 
@@ -40,8 +42,38 @@ class _AdminPageState extends State<AdminPage> {
 
     if (adminUser != null) {
       await FirebaseFirestore.instance.collection('users').doc(userId).delete();
-      // Jeśli chcesz usunąć użytkownika z Firebase Authentication, musisz wykonać to jako admin za pomocą Firebase Admin SDK.
       setState(() {});
+    } else {
+      print("Admin user is not logged in.");
+    }
+  }
+
+  void updateUser(
+      String userId, String email, String name, String password) async {
+    User? adminUser = _auth.currentUser;
+
+    if (adminUser != null) {
+      try {
+        // Aktualizacja danych użytkownika w Firestore
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(userId)
+            .update({
+          'email': email,
+          'name': name,
+        });
+
+        // Aktualizacja danych użytkownika w Firebase Authentication
+        User? user = await _auth.currentUser;
+        if (user != null) {
+          await user.updateEmail(email);
+          await user.updatePassword(password);
+        }
+
+        setState(() {});
+      } catch (e) {
+        print("Error: $e");
+      }
     } else {
       print("Admin user is not logged in.");
     }
@@ -68,12 +100,17 @@ class _AdminPageState extends State<AdminPage> {
                   decoration: InputDecoration(labelText: 'Password'),
                   obscureText: true,
                 ),
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(labelText: 'Name'),
+                ),
                 ElevatedButton(
                   onPressed: () {
-                    registerUser(
-                        _emailController.text, _passwordController.text);
+                    registerUser(_emailController.text,
+                        _passwordController.text, _nameController.text);
                     _emailController.clear();
                     _passwordController.clear();
+                    _nameController.clear();
                   },
                   child: Text('Register New User'),
                 ),
@@ -99,12 +136,67 @@ class _AdminPageState extends State<AdminPage> {
 
                     return ListTile(
                       title: Text(userData['email'] ?? 'No email'),
-                      subtitle: Text(userData['role'] ?? 'No role assigned'),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          deleteUser(user.id);
-                        },
+                      subtitle: Text(userData['name'] ?? 'No name'),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit),
+                            onPressed: () {
+                              _emailController.text = userData['email'] ?? '';
+                              _nameController.text = userData['name'] ?? '';
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text('Edit User'),
+                                  content: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      TextField(
+                                        controller: _emailController,
+                                        decoration:
+                                            InputDecoration(labelText: 'Email'),
+                                      ),
+                                      TextField(
+                                        controller: _nameController,
+                                        decoration:
+                                            InputDecoration(labelText: 'Name'),
+                                      ),
+                                      TextField(
+                                        controller: _passwordController,
+                                        decoration: InputDecoration(
+                                            labelText: 'Password'),
+                                        obscureText: true,
+                                      ),
+                                    ],
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () {
+                                        updateUser(
+                                            user.id,
+                                            _emailController.text,
+                                            _nameController.text,
+                                            _passwordController.text);
+                                        Navigator.of(context).pop();
+                                        _emailController.clear();
+                                        _passwordController.clear();
+                                        _nameController.clear();
+                                      },
+                                      child: Text('Save'),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              deleteUser(user.id);
+                            },
+                          ),
+                        ],
                       ),
                     );
                   },
